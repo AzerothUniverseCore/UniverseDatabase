28831980d8d800200a22ec4f15e9daef3da2f49b
AzerothUniverseCore <azgath.gaming@gmail.com>
2026-09-22 03:59:30 +0200

    DB: Misc Update

--- sql/auc_world/version.sql
+++ sql/auc_world/version.sql
@@ -12,7 +12,7 @@ CREATE TABLE `version` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci ROW_FORMAT=DYNAMIC COMMENT='Version Notes';
 
 INSERT INTO `version` (`core_version`, `core_revision`, `db_version`, `cache_id`) VALUES
-('SyphrenaCore rev. 1cbdc50ba99f+ 2026-09-22 01:22:06 +0200 (main branch) (Win64, RelWithDebInfo, Static)', '1cbdc50ba99f+', 'TDB 335.23061', 25425);
+('SyphrenaCore rev. 2ee7074c19e2 2026-09-22 02:31:34 +0200 (main branch) (Win64, RelWithDebInfo, Static)', '2ee7074c19e2', 'TDB 335.23061', 25425);
 
 -- 1 ligne(s)
 SET FOREIGN_KEY_CHECKS = 1;
